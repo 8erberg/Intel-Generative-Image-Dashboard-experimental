2510e2e21cc6cf4568ef60ee0cae031c8f45ff8e
8erberg <jascha.achterberg@icloud.com>
2023-01-18 13:39:30 +0000

    added conditional generation to automated assessments

--- Dashboard_setup.py
+++ Dashboard_setup.py
@@ -7,7 +7,7 @@ prompt_dir_version = '230110'
 dashboard_version_code = code_version+'_pd'+prompt_dir_version
 
 # List of tasks which are automated in current version - note that each of these needs a corresponding evaluation function in Dashboard_automation_setup.py
-automated_task_list = ['Multiple object types', 'Single object','Negation','Numbers (multiple objects)','Simple arithmetic']
+automated_task_list = ['Multiple object types', 'Single object','Negation','Numbers (multiple objects)','Simple arithmetic','Conditional generation']
 
 # Import the list of prompts used in current version
 prompt_dir = pd.read_csv('data/Prompt_dir_{0}.csv'.format(prompt_dir_version))
